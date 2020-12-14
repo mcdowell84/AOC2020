1fc2d979d80d587eb89eae96e70e1b704bfdff51
Steven <steven100@gmail.com>
2020-12-14 16:45:37 +0100

    Initial commit

--- day09A2.py
+++ day09A2.py
@@ -1,6 +1,6 @@
 # oplossing gejat van reddit
 
-file = open('input/input09.txt', 'r')
+file = open('input/input09b.txt', 'r')
 input = file.readlines()
 input = [x.replace('\n', '') for x in input]
 
@@ -16,7 +16,7 @@ def valid(target, previous_25):
         return False
 
 for foo in range(25, len(input)):
-        target = int(input[foo])
-        previous_25 = input[foo-25:foo-1]
+        target = input[foo]
+        previous_25 = input[foo-25:foo]
         if valid(target, previous_25) == False:
                 print('false: index= ' + str(foo) + ' item=' + str(input[foo]))
