9098489d46dcf4abe4905be63e5a090960eba3ec
Steven <steven100@gmail.com>
2020-12-20 17:10:57 +0100

    Initial commit

--- scratchboard.py
+++ scratchboard.py
@@ -1,18 +1,3 @@
-for diry in [-1, 0, 1]:
-    print('==============')
-    print('diry=' + str(diry))
-    print('-----------')
-    for dirx in [-1, 0, 1]:
-        print('dirx=' + str(dirx))
-        if diry == 0 and dirx == 0:
-            print(str(diry) + ' ' + str(dirx) + ' skipped')
-            continue
-        print('OK!')
-        stop = 0
-        while stop < 9:
-            if stop == 3:
-                print('triggered at 3')
-                stop += 4
-                continue
-            stop += 1
-            print('stopvalue: ' + str(stop))
+for i in [90,180,360]:
+    j = i/90
+    print(str(j))
